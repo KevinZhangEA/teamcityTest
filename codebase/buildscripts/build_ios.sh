e757c55d7d7eb166ba9c3579fb7b05600994e1af
Kevin Zhang <zhang.kevin.sg@gmail.com>
2025-08-31 14:29:58 +0800

    fix ios build issue.

--- codebase/buildscripts/build_ios.sh
+++ codebase/buildscripts/build_ios.sh
@@ -4,7 +4,6 @@ _script_name=$(basename "$0" .sh)
 source "$(dirname "$0")/_build_common.sh" start "${_script_name}"
 source "$(dirname "$0")/env.sh"
 
-# 简化生成 placeholder.out，内容为 build number
 : "${BUILD_NUMBER:=unknown}"
 echo "$BUILD_NUMBER" > placeholder.out
 
