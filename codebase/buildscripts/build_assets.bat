4f65f8e4124b09534f764f20a4c53f6cff863e8b
Kevin Zhang <zhang.kevin.sg@gmail.com>
2025-08-31 13:07:18 +0800

    generate a placeholder.out as the build target we should submit to VCS.

--- codebase/buildscripts/build_assets.bat
+++ codebase/buildscripts/build_assets.bat
@@ -4,7 +4,12 @@ for %%F in (%~nx0) do set "_script_name=%%~nF"
 call "%~dp0_build_common.bat" start %_script_name%
 call "%~dp0env.bat"
 
-rem ...existing code...
+rem 简化生成 placeholder.out，内容为 build number
+if defined BUILD_NUMBER (
+  echo %BUILD_NUMBER% > placeholder.out
+) else (
+  echo unknown > placeholder.out
+)
 
 call "%~dp0_build_common.bat" end %_script_name%
 endlocal
